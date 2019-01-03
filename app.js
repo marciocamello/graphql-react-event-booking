e7854a1b3e3f0e2d31b5a660207703c48d41f8c7
Marcio Camello <marciocamello@outlook.com>
2019-01-03 14:11:47 -0200

    Adding Relations

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema} = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -23,6 +25,12 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!,
+            email: String!
+            password: String
+        }
+
         input EventInput { 
             title: String!
             description: String!
@@ -30,12 +38,19 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
+            users: [User!]!
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -47,32 +62,104 @@ app.use('/graphql', graphqlHttp({
         events: () => {
             return Event.find()
                 .then(events => {
+
                     return events.map(event => {
+
                         return { ...event._doc, _id: event._doc._id.toString() };
                     });
                 })
                 .catch(err => {
-                    console.log('Error after list Events', err);
+
+                    console.log('Error after list Events.', err);
                     throw err;
                 });
         },
-        createEvent: (args) => {
+        users: () => {
+            return User.find()
+                .then(users => {
+
+                    return users.map(user => {
+
+                        return { ...user._doc, password: null, _id: user._doc._id.toString() };
+                    });
+                })
+                .catch(err => {
+
+                    console.log('Error after list Users.', err);
+                    throw err;
+                });
+        },
+        createEvent: args => {
 
             const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price, 
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '5c2e2f9f0b2f1937f64399fe'
             });
+
+            let createdEvent;
 
             return event.save()
                 .then(result => {
-                    return { ...result._doc, _id: event._doc._id.toString() };
+
+                    createdEvent = { ...result._doc, _id: event._doc._id.toString() };
+                    return User.findById('5c2e2f9f0b2f1937f64399fe');
+                })
+                .then(user => {
+
+                    if (!user) {
+                        
+                        throw new Error('User not found.');
+                    }
+
+                    user.createdEvents.push(event);
+
+                    return user.save();
+                })
+                .then(() => {
+
+                    return createdEvent;
                 })
                 .catch(err => {
-                    console.log('Error after save Event', err);
+
+                    console.log('Error after save Event.', err);
                     throw err;
                 });
+        },
+        createUser: args => {
+
+            return User.findOne({
+                email: args.userInput.email
+            })
+            .then(user => {
+
+                if (user) {
+
+                    throw new Error('User exists already.');
+                }
+
+                return bcrypt.hash(args.userInput.password, 12);
+            })
+            .then(hashedPassword => {
+
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+
+                return user.save();;
+            })
+            .then(result => {
+
+                return { ...result._doc, password: null, _id: result._id.toString() };
+            })
+            .catch(err => {
+
+                console.log('Error after save User.', err);
+                throw err;
+            });
         }
     },
     graphiql: true
@@ -83,9 +170,11 @@ mongoose
         `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@macbr1-vcas5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
     )
     .then(() => {
+
         console.log('Server connected on port 3000');
         app.listen(3000);
     })
     .catch( err => {
+
         console.log('Mongo Connection Error', err);
     });
